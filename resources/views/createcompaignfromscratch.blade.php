9caa3cd8e3dcdaefdca1038f842720ea76e1ecb6
unknown <tariq.mahesar@gmail.com>
2024-03-22 00:06:09 +0500

    Update Create Campaign From Scratch using Drag and Drop

--- resources/views/createcompaignfromscratch.blade.php
+++ resources/views/createcompaignfromscratch.blade.php
@@ -24,53 +24,97 @@
                     <div class="row crt_cmp_r sequence-steps">
                         <div class="col-lg-9 drop-pad">
                             <h5>Sequence Steps</h5>
-                            <div class="task-list"></div>
+                            <div class="task-list">
+                                <div class="step-1" id="step-1">
+                                    <div class="list-icon">
+                                        <i class="fa-solid fa-certificate"></i>
+                                    </div>
+                                    <div class="item_details">
+                                        <p class="item_name">Lead Source (Step 1)</p>
+                                        <p class="item_desc">Lorem ipsum dolor sit amet consectetur
+                                            adipisicing elit.</p>
+                                    </div>
+                                    <div class="attach-elements-out"></div>
+                                </div>
+                            </div>
                         </div>
-                        <div class="col-lg-3 element-list">
-                            <h5>Add Elements</h5>
-                            <ul class='drop-list'>
-                                <li>
-                                    <div class="element" id="view_profile" data-filter-item data-filter-name="view_profile">
-                                        <div class="attach-elements attach-elements-in"></div>
-                                        <div class="cancel-icon">
-                                            <i class="fa-solid fa-x"></i>
+                        <div class="col-lg-3 add-elements">
+                            <div class="element-tab">
+                                <button class="element-btn active" id="element-list-btn" data-tab="element-list">Add
+                                    Elements</button>
+                                <button class="element-btn" id="properties-btn" data-tab="properties">Properties</button>
+                            </div>
+                            <div class="element-list element-content active" id="element-list">
+                                <ul class='drop-list'>
+                                    <li>
+                                        <div class="element" id="view_profile" data-filter-item
+                                            data-filter-name="view_profile">
+                                            <div class="attach-elements attach-elements-in"></div>
+                                            <div class="cancel-icon">
+                                                <i class="fa-solid fa-x"></i>
+                                            </div>
+                                            <div class="list-icon">
+                                                <i class="fa-solid fa-eye"></i>
+                                            </div>
+                                            <div class="item_details">
+                                                <p class="item_name">View Profile</p>
+                                                <p class="item_desc">Lorem ipsum dolor sit amet consectetur
+                                                    adipisicing elit.</p>
+                                            </div>
+                                            <div class="menu-icon">
+                                                <i class="fa-solid fa-bars"></i>
+                                            </div>
+                                            <div class="attach-elements attach-elements-out"></div>
                                         </div>
-                                        <div class="list-icon">
-                                            <i class="fa-solid fa-eye"></i>
+                                    </li>
+                                    <li>
+                                        <div class="element" id="invite_to_connect" data-filter-item
+                                            data-filter-name="invite_to_connect">
+                                            <div class="attach-elements attach-elements-in"></div>
+                                            <div class="cancel-icon">
+                                                <i class="fa-solid fa-x"></i>
+                                            </div>
+                                            <div class="list-icon">
+                                                <i class="fa-solid fa-share"></i>
+                                            </div>
+                                            <div class="item_details">
+                                                <p class="item_name">Invite to Connect</p>
+                                                <p class="item_desc">Lorem ipsum, dolor sit amet consectetur
+                                                    adipisicing elit.</p>
+                                            </div>
+                                            <div class="menu-icon">
+                                                <i class="fa-solid fa-bars"></i>
+                                            </div>
+                                            <div class="attach-elements attach-elements-out"></div>
                                         </div>
-                                        <div class="item_details">
-                                            <p class="item_name">View Profile</p>
-                                            <p class="item_desc">Lorem ipsum dolor sit amet consectetur
-                                                adipisicing elit.</p>
+                                    </li>
+                                    <li>
+                                        <div class="element" id="message" data-filter-item data-filter-name="message">
+                                            <div class="attach-elements attach-elements-in"></div>
+                                            <div class="cancel-icon">
+                                                <i class="fa-solid fa-x"></i>
+                                            </div>
+                                            <div class="list-icon">
+                                                <i class="fa-solid fa-message"></i>
+                                            </div>
+                                            <div class="item_details">
+                                                <p class="item_name">Message</p>
+                                                <p class="item_desc">Lorem ipsum dolor sit amet consectetur
+                                                    adipisicing elit.</p>
+                                            </div>
+                                            <div class="menu-icon">
+                                                <i class="fa-solid fa-bars"></i>
+                                            </div>
+                                            <div class="attach-elements attach-elements-out"></div>
                                         </div>
-                                        <div class="menu-icon">
-                                            <i class="fa-solid fa-bars"></i>
-                                        </div>
-                                        <div class="attach-elements attach-elements-out"></div>
-                                    </div>
-                                </li>
-                                <li>
-                                    <div class="element" id="invite_to_connect" data-filter-item
-                                        data-filter-name="invite_to_connect">
-                                        <div class="attach-elements attach-elements-in"></div>
-                                        <div class="cancel-icon">
-                                            <i class="fa-solid fa-x"></i>
-                                        </div>
-                                        <div class="list-icon">
-                                            <i class="fa-solid fa-share"></i>
-                                        </div>
-                                        <div class="item_details">
-                                            <p class="item_name">Invite to Connect</p>
-                                            <p class="item_desc">Lorem ipsum, dolor sit amet consectetur
-                                                adipisicing elit.</p>
-                                        </div>
-                                        <div class="menu-icon">
-                                            <i class="fa-solid fa-bars"></i>
-                                        </div>
-                                        <div class="attach-elements attach-elements-out"></div>
-                                    </div>
-                                </li>
-                            </ul>
+                                    </li>
+                                </ul>
+                                <div class="save-btns">
+                                    <button id="save-changes">Save Changes</button>
+                                </div>
+                            </div>
+                            <div class="properties element-content" id="properties">
+                            </div>
                         </div>
                     </div>
                 </div>
--- resources/views/partials/dashboard_header.blade.php
+++ resources/views/partials/dashboard_header.blade.php
@@ -37,9 +37,13 @@
         </script>
         <script>
             $(document).ready(function() {
+                $('.attach-elements-out').on('click', attachElementOutput);
+
                 var chooseElement;
                 var elementInput;
                 var elementOutput;
+                var count = 0;
+                var final_array = [];
 
                 function move() {
                     $('.element').on('mousedown', function(e) {
@@ -49,6 +53,10 @@
                         });
                         $('body').append(clone);
                         chooseElement = clone;
+                        id = chooseElement.attr('id') + '_' + ++count;
+                        chooseElement.attr('id', id);
+                        chooseElement.attr('class', 'drop-pad-element');
+                        chooseElement.removeClass('element');
                         var task_list = $('.task-list').get(0).getBoundingClientRect();
                         $(document).on('mousemove', function(e) {
                             var x = e.pageX;
@@ -71,49 +79,74 @@
                         if (chooseElement) {
                             $(document).off('mousemove');
                             $('.task-list').append(chooseElement);
-                            chooseElement.on('mousedown', startDragging);
                             $('.cancel-icon').on('click', removeElement);
-                            $('.attach-elements-out').on('click', attachElementInput);
-                            $('.attach-elements-in').on('click', attachElementOutput)
+                            $('.drop-pad-element').on('click', elementProperties);
+                            $('.attach-elements-out').on('click', attachElementOutput);
+                            $('.attach-elements-in').on('click', attachElementInput)
+                            chooseElement.on('mousedown', startDragging);
                             chooseElement = null;
                         }
                     });
                 }
 
-                function attachElementInput(e) {
-                    var attachDiv = $(this);
-                    attachDiv.css({
-                        "background-color": "white"
-                    });
-                    elementInput = attachDiv[0];
+                function elementProperties(e) {
+                    $('#properties').empty();
+                    var item = $(this);
+                    var item_name = item.find('.item_name').text();
+                    var list_icon = item.find('.list-icon').html();
+                    var name_html = '<div class="element_properties">' + list_icon + '<p>' + item_name + '</p></div>';
+                    $('#properties').append(name_html);
+                    $('#element-list').removeClass('active');
+                    $('#properties').addClass('active');
+                    $('#element-list-btn').removeClass('active');
+                    $('#properties-btn').addClass('active');
                 }
 
                 function attachElementOutput(e) {
-                    var attachDiv = $(this);
-                    attachDiv.css({
-                        "background-color": "white"
-                    });
-                    elementOutput = attachDiv[0];
+                    if (!elementInput) {
+                        var attachDiv = $(this);
+                        attachDiv.css({
+                            "background-color": "white"
+                        });
+                        elementOutput = attachDiv.parent();
+                    }
+                }
 
-                    if (elementInput && elementOutput) {
-                        var svgNS = "http://www.w3.org/2000/svg";
-                        var svg = document.createElementNS(svgNS, "svg");
-                        var line = document.createElementNS(svgNS, "line");
-                        var rect1 = elementInput.getBoundingClientRect();
-                        var rect2 = elementOutput.getBoundingClientRect();
-                        var x1 = rect1.left + rect1.width / 2;
-                        var y1 = rect1.top + rect1.height / 2;
-                        var x2 = rect2.left + rect2.width / 2;
-                        var y2 = rect2.top + rect2.height / 2;
-                        console.log("x1:", x1, "y1:", y1, "x2:", x2, "y2:", y2);
-                        line.setAttribute("x1", x1);
-                        line.setAttribute("y1", y1);
-                        line.setAttribute("x2", x2);
-                        line.setAttribute("y2", y2);
-                        line.setAttribute("stroke", "pink");
-                        line.setAttribute("stroke-width", "25");
-                        svg.appendChild(line);
-                        document.body.appendChild(svg);
+                function attachElementInput(e) {
+                    if (elementOutput && elementOutput.attr('id') != $(this).parent().attr('id')) {
+                        var attachDiv = $(this);
+                        attachDiv.css({
+                            "background-color": "white"
+                        });
+                        elementInput = attachDiv.parent();
+                        if (elementInput && elementOutput) {
+                            if (!final_array.includes(elementOutput.attr('id'))) {
+                                final_array.push(elementOutput.attr('id'));
+                                final_array.push(elementInput.attr('id'));
+                            } else if (final_array.includes(elementOutput.attr('id'))) {
+                                final_array.push(elementInput.attr('id'));
+                            }
+                        }
+                        $('.task-list').append('<div class="line" id="' + elementOutput.attr('id') + '-' + elementInput
+                            .attr('id') +
+                            '"></div>');
+                        var attachElementInput = elementInput.find('.attach-elements-in');
+                        var attachElementOutput = elementOutput.find('.attach-elements-out');
+                        var rect1 = attachElementOutput.get(0).getBoundingClientRect();
+                        var rect2 = attachElementInput.get(0).getBoundingClientRect();
+                        if (rect1 && rect2) {
+                            var x1 = rect1.left + (rect1.width / 2);
+                            var y1 = rect1.top + (rect1.height / 2);
+                            var x2 = rect2.left + (rect2.width / 2);
+                            var y2 = rect2.top + (rect2.height / 2);
+                            console.log(rect1);
+                            console.log(rect2);
+                            var lineId = elementOutput.attr('id') + '-' + elementInput.attr('id');
+                            create_line(x1, y1, x2, y2, lineId);
+                        }
+
+                        elementInput = null;
+                        elementOutput = null;
                     }
                 }
 
@@ -141,6 +174,35 @@
                     });
                 }
                 move();
+                $('.element-btn').on('click', function() {
+                    var targetTab = $(this).data('tab');
+                    $('.element-content').removeClass('active');
+                    $('#' + targetTab).addClass('active');
+                    $('.element-btn').removeClass('active');
+                    $(this).addClass('active');
+                });
+                $('#save-changes').on('click', function() {
+                    if (final_array[0] != 'step-1') {
+                        alert('Select Step 1 First');
+                        location.reload();
+                    } else {
+                        console.log(final_array);
+                    }
+                });
+
+                function create_line(x1, y1, x2, y2, lineId) {
+                    var line = $('#' + lineId);
+                    line.css({
+                        'position': 'absolute',
+                        'background': 'none',
+                        'border-radius': 0,
+                        'border-right': '3px solid black',
+                        'left': x1 + 'px',
+                        'top': y1 + 'px',
+                        'width': 1 + 'px',
+                        'height': (y2 - y1) + 'px',
+                    });
+                }
             });
         </script>
     </footer>
